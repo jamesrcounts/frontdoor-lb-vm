3bee29ab06035edf422608c1e89924e164b4cf7b
Jim Counts <jamesrcounts@outlook.com>
2020-10-27 23:45:41 +0000

    Add load balancer

--- infrastructure/azure/front-door.tf
+++ infrastructure/azure/front-door.tf
@@ -1,43 +1,50 @@
+locals {
+  frontend_host                    = "${local.project}.azurefd.net"
+  frontend_endpoint_name           = replace(local.frontend_host, ".", "")
+  backend_pool_load_balancing_name = "${local.project}-backend-pool-load-balancing"
+  backend_pool_health_probe_name   = "${local.project}-backend-pool-health-probe"
+}
+
 resource "azurerm_frontdoor" "example" {
   name                                         = local.project
   resource_group_name                          = azurerm_resource_group.example.name
   enforce_backend_pools_certificate_name_check = false
 
   routing_rule {
-    name               = "exampleRoutingRule1"
+    name               = "fd-to-lb"
     accepted_protocols = ["Http", "Https"]
     patterns_to_match  = ["/*"]
-    frontend_endpoints = ["exampleFrontendEndpoint1"]
+    frontend_endpoints = [local.frontend_endpoint_name]
     forwarding_configuration {
       forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "exampleBackendBing"
+      backend_pool_name   = azurerm_public_ip.example.name
     }
   }
 
   backend_pool_load_balancing {
-    name = "exampleLoadBalancingSettings1"
+    name = local.backend_pool_load_balancing_name
   }
 
   backend_pool_health_probe {
-    name = "exampleHealthProbeSetting1"
+    name = local.backend_pool_health_probe_name
   }
 
   backend_pool {
-    name = "exampleBackendBing"
+    name = azurerm_public_ip.example.name
     backend {
-      host_header = "www.bing.com"
-      address     = "www.bing.com"
+      enabled     = true
+      address     = azurerm_public_ip.example.ip_address
+      host_header = local.frontend_host
       http_port   = 80
       https_port  = 443
     }
-
-    load_balancing_name = "exampleLoadBalancingSettings1"
-    health_probe_name   = "exampleHealthProbeSetting1"
+    load_balancing_name = local.backend_pool_load_balancing_name
+    health_probe_name   = local.backend_pool_health_probe_name
   }
 
   frontend_endpoint {
-    name      = "exampleFrontendEndpoint1"
-    host_name = "${local.project}.azurefd.net"
+    name      = local.frontend_endpoint_name
+    host_name = local.frontend_host
   }
 }
 
