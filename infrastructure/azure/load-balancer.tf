efc2ed3967296717e53d0c1dfedfb11ebc458dec
Jim Counts <jamesrcounts@outlook.com>
2020-10-28 05:08:34 +0000

    Upgrade LB sku

--- infrastructure/azure/load-balancer.tf
+++ infrastructure/azure/load-balancer.tf
@@ -7,12 +7,14 @@ resource "azurerm_public_ip" "example" {
   location            = azurerm_resource_group.example.location
   name                = "pip-${local.project}"
   resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
 }
 
 resource "azurerm_lb" "example" {
   location            = azurerm_resource_group.example.location
   name                = "lb-${local.project}"
   resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = local.frontend_ip_configuration_name
